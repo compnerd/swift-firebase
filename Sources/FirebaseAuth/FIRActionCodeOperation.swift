900221b7b9b9e36fc2b9291f604c0133e8ea9887
Saleem Abdulrasool <compnerd@compnerd.org>
2023-08-20 11:31:57 -0700

    FirebaseAuth: rename `ActionCodeOperation` enumerations
    These should be prefix stripped and lowercased as per the
    omit-needless-words approach.

--- Sources/FirebaseAuth/FIRActionCodeOperation.swift
+++ Sources/FirebaseAuth/FIRActionCodeOperation.swift
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: BSD-3-Clause
 
 public enum ActionCodeOperation: Int {
-  case FIRActionCodeOperationUnknown
-  case FIRActionCodeOperationPasswordReset
-  case FIRActionCodeOperationVerifyEmail
-  case FIRActionCodeOperationRecoverEmail
-  case FIRActionCodeOperationEmailLink
-  case FIRActionCodeOperationVerifyAndChangeEmail
-  case FIRActionCodeOperationRevertSecondFactorAddition
+  case unknown
+  case passwordReset
+  case verifyEmail
+  case recoverEmail
+  case emailLink
+  case verifyAndChangeEmail
+  case revertSecondFactorAddition
 }
