3471c58bd99e72dfa8b37990ffe5f1cd347151f5
Brian Michel <brian@thebrowser.company>
2024-01-03 17:10:12 -0500

    Move the snapshot callback to the main queue (#15)
    * Move the snapshot callback to the main queue
    
    * Add comment for future us

--- Sources/FirebaseFirestore/DocumentReference+Swift.swift
+++ Sources/FirebaseFirestore/DocumentReference+Swift.swift
@@ -63,7 +63,16 @@ extension DocumentReference {
           let error = NSError.firestore(errorCode)
           // We only return a snapshot if the error code isn't 0 (aka the 'ok' error code)
           let returned = error == nil ? snapshot?.pointee : nil
-          callback(returned, error)
+
+          // Make sure we dispatch our callback back into the main thread to keep consistent
+          // with the reference API which will call back on the 'user_executor' which typically
+          // ends up being the main queue.
+          // Relevant code:
+          // - https://github.com/firebase/firebase-ios-sdk/blob/main/Firestore/Source/API/FIRFirestore.mm#L210-L218
+          // - https://github.com/firebase/firebase-ios-sdk/blob/main/Firestore/core/src/api/document_reference.cc#L236-L237
+          DispatchQueue.main.async {
+            callback(returned, error)
+          }
         }
       }, UnsafeMutableRawPointer(boxed.toOpaque()))
 
