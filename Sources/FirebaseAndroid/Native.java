91335e00f3b6b1f2a557da04fbd559568c2d091e
Saleem Abdulrasool <abdulras@thebrowser.company>
2024-04-02 09:42:40 -0700

    FirebaseAndroid: introduce `SwiftFirebase` JAR
    This introduces the Java bindings for the JNI backed implementation.
    When implementing an Android application which uses swift-firebase,
    `SwiftFirebase` provides the bridge to the JNI functions required to
    enable the functionality.

---
+++ Sources/FirebaseAndroid/Native.java
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: BSD-3-Clause
+
+package company.thebrowser;
+
+import android.app.Activity;
+
+public class Native
+{
+  public native boolean RegisterActivity(android.app.Activity activity);
+}
